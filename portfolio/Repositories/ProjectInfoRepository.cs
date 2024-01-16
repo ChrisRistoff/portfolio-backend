f5a05b69fe5a815abbe5b8e2439599db67e170cb
Krasyo <doanusho@gmail.com>
2024-01-16 22:17:48 +0000

    error handling Handle not found errors in get project by id and update project by id endpoints

--- portfolio/Repositories/ProjectInfoRepository.cs
+++ portfolio/Repositories/ProjectInfoRepository.cs
@@ -95,7 +95,12 @@ public class ProjectInfoRepository
 
         var sql = "SELECT * FROM projects WHERE id = @ProjectId";
 
-        var result = await connection.QueryFirstAsync<dynamic>(sql, new { ProjectId = projectId });
+        var result = await connection.QueryFirstOrDefaultAsync<dynamic>(sql, new { ProjectId = projectId });
+
+        if (result == null)
+        {
+            return null!;
+        }
 
         return new ProjectInfoModel
         {
@@ -109,6 +114,50 @@ public class ProjectInfoRepository
             TechStack = ((string[])result.tech_stack)?.ToArray(),
             Type = result.project_type
         };
+    }
 
+    public async Task<ProjectInfoModel?> UpdateProject(int projectId, UpdateProjectDto project)
+    {
+        await using var connection = new NpgsqlConnection(_connectionString);
+
+        string findProject = "SELECT * FROM projects WHERE id = @ProjectId";
+
+        var projectToUpdate = await connection.QueryFirstOrDefaultAsync<ProjectInfoModel>(findProject, new { ProjectId = projectId });
+
+        if (projectToUpdate == null)
+        {
+            return null;
+        }
+
+        DynamicParameters parameters = new DynamicParameters(project);
+        parameters.Add("ProjectId", projectId);
+
+        string sql = @"UPDATE projects
+                        SET id = @Id,
+                            name = @Name,
+                            tagline = @Tagline,
+                            description = @Description,
+                            image = @Image,
+                            repo = @Repo,
+                            link = @Link,
+                            tech_stack = @TechStack,
+                            project_type = @Type
+                        WHERE id = @ProjectId
+                        RETURNING *";
+
+        var result = await connection.QueryFirstOrDefaultAsync<dynamic>(sql, parameters);
+
+        return new ProjectInfoModel
+        {
+            Id = result.id,
+            Name = result.name,
+            Tagline = result.tagline,
+            Description = result.description,
+            Image = result.image,
+            Repo = result.repo,
+            Link = result.link,
+            TechStack = ((string[])result.tech_stack)?.ToArray(),
+            Type = result.project_type
+        };
     }
 }
